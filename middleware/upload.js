030cbf7062b0c0d50413446888ac1a979dc44588
lancherix <lancherix.service@gmail.com>
2025-12-23 20:18:38 -0500

    uploads

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,19 +4,10 @@ import cloudinary from "../config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: async (req, file) => {
-    if (req.originalUrl.includes("/board-images")) {
-      return {
-        folder: `board/${req.params.projectId}`,
-        allowed_formats: ["jpg", "png", "jpeg", "webp"],
-      };
-    }
-
-    return {
-      folder: "profile_pictures",
-      allowed_formats: ["jpg", "png", "jpeg", "webp"],
-    };
-  }
+  params: {
+    folder: "profile_pictures",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+  },
 });
 
 const upload = multer({ storage });
